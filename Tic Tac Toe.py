da45f7f599a9d1581a5a55b1f96a5f702de4cd85
Nduta Red <agnesndutamwaniki@gmail.com>
2020-07-16 19:07:52 +0300

    add the images and Python file that we will use in the project

---
+++ Tic Tac Toe.py
@@ -0,0 +1,132 @@
+#import tkinter to create GUI
+from tkinter import *
+
+#help us show the winner or draw
+import tkinter.messagebox
+
+#create an instance of Tk in order to use Tk methods
+root = Tk()
+
+#changing the title
+root.title('Tic Tac Toe')
+
+#prevent the user from resizing the window
+root.resizable(False,False)
+
+#create global variables
+click = True
+
+#counts the number of moves. If there are 9 moves, then it's a draw
+count = 0
+
+#create string variables which will be associated with user input
+btn1 = StringVar()
+btn2 = StringVar()
+btn3 = StringVar()
+btn4 = StringVar()
+btn5 = StringVar()
+btn6 = StringVar()
+btn7 = StringVar()
+btn8 = StringVar()
+btn9 = StringVar()
+
+#store x and o into variable
+xPhoto = PhotoImage(file='x.png')
+oPhoto = PhotoImage(file='o.png')
+
+#main game function
+def game():
+    button1 = Button(root, height=9, width=19,relief='ridge',borderwidth=5,background='#EFBBFF', textvariable=btn1,command=lambda:btn_press(1,0,0))
+    button1.grid(row=0,column=0)
+
+    button2 = Button(root, height=9, width=19,relief='ridge',borderwidth=5,background='#EFBBFF', textvariable=btn2,command=lambda:btn_press(2,0,1))
+    button2.grid(row=0,column=1)
+
+    button3 = Button(root, height=9, width=19,relief='ridge',borderwidth=5,background='#EFBBFF', textvariable=btn3,command=lambda:btn_press(3,0,2))
+    button3.grid(row=0,column=2)
+
+    button4 = Button(root, height=9, width=19,relief='ridge',borderwidth=5,background='#D896FF', textvariable=btn4,command=lambda:btn_press(4,1,0))
+    button4.grid(row=1,column=0)
+
+    button5 = Button(root, height=9, width=19,relief='ridge',borderwidth=5,background='#D896FF', textvariable=btn5,command=lambda:btn_press(5,1,1))
+    button5.grid(row=1,column=1)
+
+    button6 = Button(root, height=9, width=19,relief='ridge',borderwidth=5,background='#D896FF', textvariable=btn6,command=lambda:btn_press(6,1,2))
+    button6.grid(row=1,column=2)
+
+    button7 = Button(root, height=9, width=19,relief='ridge',borderwidth=5,background='#BE29EC', textvariable=btn7,command=lambda:btn_press(7,2,0))
+    button7.grid(row=2,column=0)
+
+    button8 = Button(root, height=9, width=19,relief='ridge',borderwidth=5,background='#BE29EC', textvariable=btn8,command=lambda:btn_press(8,2,1))
+    button8.grid(row=2,column=1)
+
+    button9 = Button(root, height=9, width=19,relief='ridge',borderwidth=5,background='#BE29EC', textvariable=btn9,command=lambda:btn_press(9,2,2))
+    button9.grid(row=2,column=2)
+
+#function to check the pressed button
+def btn_press(num,r,c):
+    global count,click
+
+    while click:
+        labelPhoto = Label(root, image = xPhoto)
+        labelPhoto.grid(row = r, column = c)
+        
+        if num == 1:
+            btn1.set('x')
+        elif num == 2:
+            btn2.set('x') 
+        elif num == 3:
+            btn3.set('x')
+        elif num == 4:
+            btn4.set('x')
+        elif num == 5:
+            btn5.set('x') 
+        elif num == 6:
+            btn6.set('x')
+        elif num == 7:
+            btn7.set('x')
+        elif num == 8:
+            btn8.set('x')
+        else:
+            btn9.set('x')
+        
+        click = False
+        count += 1
+
+    while not click:
+        labelPhoto = Label(root, image = oPhoto)
+        labelPhoto.grid(row = r, column = c)
+
+        if num == 1:
+            btn1.set('o')
+        elif num == 2:
+            btn2.set('o') 
+        elif num == 3:
+            btn3.set('o')
+        elif num == 4:
+            btn4.set('o')
+        elif num == 5:
+            btn5.set('o') 
+        elif num == 6:
+            btn6.set('o')
+        elif num == 7:
+            btn7.set('o')
+        elif num == 8:
+            btn8.set('o')
+        else:
+            btn9.set('o')
+        
+        click = True
+        count += 1  
+game()                    
+
+#function to check for a winner
+def checkWinner():
+    pass
+
+#function to clear game
+def clear_game():
+    pass
+
+#event handler
+root.mainloop()
